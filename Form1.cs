51345a7155750d6556e32c030d83a5ebc19202a3
derickcux2023 <derickcux2023@LAB4-20>
2024-02-01 08:26:09 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlPersonas
+{
+    public partial class Form1 : Form
+    {
+        Persona persona = new Persona(); //Instanciar una clase, crear un objeto de esa clase.
+        string Nombre;
+        string Apellido;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnIngreso_Click(object sender, EventArgs e)
+        {
+            
+            persona.Dpi = TxtDpi.Text;
+            persona.Nombre = txtNombre.Text;
+            persona.Apellido = TxtApe.Text;
+            persona.FechaNacimiento = dateTimePicker1.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnMostrar_Click(object sender, EventArgs e)
+        {
+            LblDpi.Text = persona.Dpi;
+            LblNom.Text = persona.Nombre;
+            LblApe.Text = persona.Apellido;
+            LblFe.Text = persona.FechaNacimiento.ToShortDateString();
+            labelEdad.Text = persona.edad().ToString();
+
+            LblDpi.Visible = true;
+            LblNom.Visible = true;
+            LblApe.Visible = true;
+            LblFe.Visible = true;
+            labelEdad.Visible = true;
+        }
+
+        private void buttonInicialMayus_Click(object sender, EventArgs e)
+        {
+            persona.InicialMayus();
+            LblNom.Text = persona.Nombre;
+            LblApe.Text = persona.Apellido;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Persona.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlPersonas
+{
+    class Persona
+    {
+        //String S mayuscula Trae varias funciones con el punto
+        //string s minuscula  solo es para declarar variable
+        //Campos y Propiedades
+        //Campos, Privasdos para cumplir con el encapsulado
+        String dpi;
+        String nombre;
+        String apellido;
+        DateTime fechaNacimiento;
+        int otroDato;
+
+        //Encapsulado
+        public string Dpi { get => dpi; set => dpi = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public int OtroDato { get => otroDato; set => otroDato = value; }
+
+        //Constructor de la clase:
+        //Código que se ejecuta al crearse un objeto de esta clase
+        //Por ejemplo inicializar variables
+        public Persona()
+        {
+            dpi = "";
+            nombre = "";
+            apellido = "";
+            fechaNacimiento = DateTime.Now;
+        }
+
+
+        //Métodos: Acciones que se realizan
+        public void InicialMayus ()
+        {
+            if (nombre.Length > 0)
+            {
+                StringBuilder nombreMasyucula = new StringBuilder(nombre);
+                nombreMasyucula[0] = char.ToUpper(nombreMasyucula[0]);
+                nombre = nombreMasyucula.ToString();
+            }
+
+            if (apellido.Length > 0)
+            {
+                StringBuilder apellidoMayuscula = new StringBuilder(apellido);
+                apellidoMayuscula[0] = char.ToUpper(apellidoMayuscula[0]);
+                apellido = apellidoMayuscula.ToString();
+            }
+        }
+
+        public string normalizaNombre()
+        {
+            
+            return "hola";
+        }
+
+        public int edad()
+        {
+            int edad = 0;
+            //Calcular edad
+            int mesNacimiento = fechaNacimiento.Month;
+            int añoNacimiento = fechaNacimiento.Year;
+            int diaNacimiento = fechaNacimiento.Day;
+
+            int mesActual = DateTime.Now.Month;
+            int añoActual = DateTime.Now.Year;
+            int diaActual = DateTime.Now.Day;
+
+            if (mesActual > mesNacimiento)
+            {
+                edad = (añoActual - añoNacimiento);
+                
+            }
+            else if ((mesActual >= mesNacimiento)&&(diaActual >= diaNacimiento))
+            {
+                edad = (añoActual - añoNacimiento);
+            }
+            else
+            {
+                edad = (añoActual - añoNacimiento) - 1;
+            }
+            return edad;
+        }
+
+
+        //Propiedades
+        //get  obtiene el valor 
+        //a = DPI;
+
+        // set cuando se le asigna un valor 
+
+
+    }
+}
